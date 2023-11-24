1b2e1083295ac6c5c367d162e1ed1d09e436c186
Mikhail Aleksandrov <aleksandrov.m2542@gmail.com>
2023-11-24 17:13:06 +0300

    tgstat_parallel_parser_launcher (#45)

---
+++ src/parsers/tgstat/launcher.py
@@ -0,0 +1,69 @@
+import sys
+from datetime import datetime
+from multiprocessing import Process
+from loguru import logger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from config import LOGGER_LEVEL, PROCESS_LOGGER_FORMAT
+from src.dao.db_config import DB_CONFIG
+from src.dao.mentions_db import MentionsDatabase, ChatContentType, Chat
+from src.dao.mentions_db import Proxy
+from src.parsers.tgstat.chat import ChannelParser
+from src.utils import divide_into_chunks, add_log_to_file_for_process
+
+
+def launch_parser(chats: list[Chat], proxy: dict[str, str]) -> None:
+    """
+    launch one parser
+    :param chats: chats to parse
+    :param proxy: proxy dict for requests library
+    """
+    if LOGGER_LEVEL == 'OFF':
+        logger.remove()
+
+    engine = create_engine(DB_CONFIG.DB_URI, echo=False)
+    session = Session(bind=engine, autoflush=False)
+    database = MentionsDatabase(session)
+    start_date = datetime.min
+    print(start_date)
+    cp = ChannelParser(start_date=start_date, database=database, proxy=proxy)
+
+    cp.process_chats(chats)
+
+    engine.dispose()
+    session.close()
+
+
+def launch_many_parsers() -> None:
+    """
+    launches separate parsers in multiple processes, waits until parsing is done
+    """
+    logger.remove()
+    if LOGGER_LEVEL != 'OFF':
+        logger.add(sys.stdout, format=PROCESS_LOGGER_FORMAT, level=LOGGER_LEVEL)
+    add_log_to_file_for_process('ChannelParserLauncher')
+
+    engine = create_engine(DB_CONFIG.DB_URI, echo=False)
+    session = Session(bind=engine, autoflush=False)
+    database = MentionsDatabase(session)
+    chats = database.get_chats_by_content_type(ChatContentType.wb_items_ads)
+    proxies = database.session.query(Proxy).all()
+    engine.dispose()
+    session.close()
+
+    processes = []
+    proxies_list = [proxy.get_http_dict() for proxy in proxies]
+    chats_chunks = list(divide_into_chunks(chats, len(proxies_list)))
+
+    for (proxy, chats_chunk) in zip(proxies_list, chats_chunks):
+        p = Process(target=launch_parser, args=[chats_chunk, proxy])
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+    logger.info('ALL PROCESSES ARE DONE')
+
+
+if __name__ == '__main__':
+    launch_many_parsers()
