a9ce3ef5cb782163ee7fb0e36a3da070c5368d4b
Alexander Makeykin <marzepanh@gmail.com>
2023-12-03 18:44:17 +0300

    bot result pagination (#47)

--- src/bot/message_handler.py
+++ src/bot/message_handler.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime
 from typing import Type
-
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -25,6 +24,10 @@ session = Session(bind=engine)
 
 db = MentionsDatabase(session=session)
 user_db = UserDatabase(session)
+
+CURRENT_PAGE_KEY = 'current_page'
+PAGES_COUNT_KEY = 'pages_count'
+REPLY_KEY = 'reply'
 
 
 @dp.message_handler(commands="start", state="*")
@@ -93,8 +96,8 @@ async def handle_entered_sku(message: types.Message, state: FSMContext) -> None:
         text = f'Артикул <i>{sku}</i> не упоминался ни в одном канале.'
     else:
         text = get_text_for_sku_response(sku, mentions)
-    await send_message_with_limit(message.from_user.id, text)
     await state.finish()
+    await send_page_result(message.from_user.id, split_message(text), 1, state)
 
 
 def get_text_for_sku_response(sku: str, mentions: dict[Chat, dict[Post, list[Type[SkuPerPost]]]]) -> str:
@@ -153,11 +156,11 @@ async def handle_entered_brand(message: types.Message, state: FSMContext) -> Non
         text = f'Ни один артикул бренда <i>{brand}</i> не упоминался ни в одном канале.'
     else:
         text = get_text_for_brand_response(brand, mentions)
-    await send_message_with_limit(message.from_user.id, text)
     await state.finish()
+    await send_page_result(message.from_user.id, split_message(text), 1, state)
 
 
-def get_text_for_brand_response(brand: str, mentions: dict[Chat, dict[Post, list[Type[SkuPerPost]]]]) -> str:
+def get_text_for_brand_response(brand: str, mentions: dict[Chat: dict[Post: list[Type[SkuPerPost]]]]) -> str:
     mentions_count = 0
     text = ''
     for chat, posts in mentions.items():
@@ -172,7 +175,7 @@ def get_text_for_brand_response(brand: str, mentions: dict[Chat, dict[Post, list
     return text
 
 
-def get_mentions_list_text_for_chat(chat: Chat, posts: dict[Post, list[Type[SkuPerPost]]]) -> (str, int):
+def get_mentions_list_text_for_chat(chat: Chat, posts: dict[Post: list[Type[SkuPerPost]]]) -> (str, int):
     text = ''
     sorted_posts = list(posts.keys())
     sorted_posts.sort(key=lambda p: p.date)
@@ -216,6 +219,73 @@ async def send_message_with_limit(user_id: int, text: str) -> None:
                                disable_web_page_preview=True)
         sent_text_len = sent_text_len + end - start + 1
         start = end + 1
+
+
+def split_message(text: str) -> list[str]:
+    splitted_text: list[str] = []
+    message_limit = 4096
+    text_len = len(text)
+    sent_text_len = 0
+    start = 0
+    while sent_text_len < text_len:
+        if (text_len - 1) - start < message_limit:
+            end = text_len - 1
+        else:
+            end = start + (message_limit - 1)
+            newline_index = find_index_of_nearest_newline(end, text)
+            if newline_index > start:
+                end = newline_index
+        text_to_send = text[start:end + 1]
+        splitted_text.append(text_to_send)
+        sent_text_len = sent_text_len + end - start + 1
+        start = end + 1
+    return splitted_text
+
+
+async def send_page_result(user_id: int, splitted_text: list[str], page_to_send: int, state: FSMContext) -> None:
+    pages_count = len(splitted_text)
+    await state.set_data({
+        CURRENT_PAGE_KEY: 1,
+        REPLY_KEY: splitted_text,
+    })
+    keyboard = get_pagination_keyboard(1, pages_count)
+    await bot.send_message(user_id, text=splitted_text[page_to_send - 1], reply_markup=keyboard, parse_mode='html',
+                           disable_web_page_preview=True)
+
+
+@dp.callback_query_handler(lambda call: call.data == 'go_to_next_page')
+async def next_page_call(call: types.callback_query, state: FSMContext) -> None:
+    data = await state.get_data()
+    splitted_text = data[REPLY_KEY]
+    pages_count = len(splitted_text)
+    page = data[CURRENT_PAGE_KEY] + 1
+    if page > pages_count:
+        page = 1
+    keyboard = get_pagination_keyboard(page, pages_count)
+    await bot.edit_message_text(chat_id=call.from_user.id, message_id=call.message.message_id,
+                                text=splitted_text[page - 1], reply_markup=keyboard, parse_mode='html',
+                                disable_web_page_preview=True)
+    await state.update_data({CURRENT_PAGE_KEY: page})
+
+
+@dp.callback_query_handler(lambda call: call.data == 'go_to_prev_page')
+async def prev_page_call(call: types.callback_query, state: FSMContext) -> None:
+    data = await state.get_data()
+    splitted_text = data[REPLY_KEY]
+    pages_count = len(splitted_text)
+    page = data[CURRENT_PAGE_KEY] - 1
+    if page == 0:
+        page = pages_count
+    keyboard = get_pagination_keyboard(page, pages_count)
+    await bot.edit_message_text(chat_id=call.from_user.id, message_id=call.message.message_id,
+                                text=splitted_text[page - 1], reply_markup=keyboard, parse_mode='html',
+                                disable_web_page_preview=True)
+    await state.update_data({CURRENT_PAGE_KEY: page})
+
+
+@dp.callback_query_handler(lambda call: call.data == 'none')
+async def none_action(call: types.callback_query, state: FSMContext) -> None:
+    await bot.answer_callback_query(call.id)
 
 
 def find_index_of_nearest_newline(end_index, text):
